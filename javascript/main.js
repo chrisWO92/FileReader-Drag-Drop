04e4151f8641c03ca328d4066f75c2d7ca59158b
cristian <chrisworkingonline@gmail.com>
2022-02-12 00:50:22 -0300

    final commit

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+import {dragOverHandler, projectDropHandler, changeColor} from "./modules/dragAndDropFunctions.js";
+
+const dropZone = document.querySelector(".drop-zone");
+const result = document.querySelector(".result");
+const text = document.querySelector(".text");
+
+
+dropZone.addEventListener("dragover", (e) => {
+    dragOverHandler(e);
+    dropZone.textContent = 'Puedes soltar';
+    changeColor(dropZone, "rgb(184, 183, 183)");
+    dropZone.style.backgroundColor = "white";
+    dropZone.style.transition = "background-color 0.3s linear 0s";
+});
+
+dropZone.addEventListener("dragleave", (e) => {
+    dragOverHandler(e);
+    dropZone.textContent = 'Arrastre aquí su archivo';
+    changeColor(dropZone, "grey");
+    dropZone.style.backgroundColor = "white";
+    dropZone.style.transition = "background-color 0.3s linear 0s";
+});
+
+dropZone.addEventListener("drop", (e) => {
+    projectDropHandler(e, result);
+    dropZone.textContent = '¡Carga completa!'
+    text.textContent = 'Archivos arrastrados';
+    changeColor(dropZone, "green");
+    dropZone.style.backgroundColor = "#8bc9a5";
+    dropZone.style.transition = "background-color 0.3s linear 0s";
+});
+
+
